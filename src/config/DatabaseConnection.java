d6978959752a1d67875513559c151f7e29387afd
Asma <asmasanjari877@gamil.com>
2026-09-21 00:58:53 -0800

    add transactiontype enum for credit transactions

--- src/config/DatabaseConnection.java
+++ src/config/DatabaseConnection.java
@@ -7,13 +7,13 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private static final String URL =
-            "jdbc:postgresql://localhost:5432/smspanel";
+            "jdbc:postgresql://localhost:5432/postgres";
 
     private static final String USER =
             "postgres";
 
     private static final String PASSWORD =
-            "1234";
+            "AasmaA";
 
     public static Connection getConnection()
             throws SQLException {
