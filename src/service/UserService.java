af316f597cb12ad6429189473d2ea6ba08c80712
Asma <asmasanjari877@gamil.com>
2026-09-21 06:05:26 -0800

    save registration gift transaction

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,5 +1,7 @@
 package service;
 
+import entity.Transaction;
+import entity.TransactionType;
 import entity.User;
 import repository.TransactionRepository;
 import repository.UserRepository;
@@ -15,11 +17,31 @@ public class UserService {
         this.transactionRepository = transactionRepository;
     }
 
-    public User register(String name, String username, String password) {
+
+    public User register(String name,
+                         String username,
+                         String password) {
+
         User existingUser =
                 userRepository.findByUsername(username);
+
         if (existingUser != null) {
+            System.out.println("Username already exists.");
             return null;
         }
+        User user = new User(name, username, password);
+
+        userRepository.save(user);
+        Transaction giftTransaction =
+                new Transaction(
+                        user.getId(),
+                        user.getCredit(),
+                        TransactionType.CREDIT,
+                        "Registration gift"
+                );
+
+        transactionRepository.save(giftTransaction);
+
+        return user;
     }
 }
