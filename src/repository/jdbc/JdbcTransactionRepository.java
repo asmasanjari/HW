aded35edd9710e2d770258be8c3960149ebf7eb8
Asma <asmasanjari877@gamil.com>
2026-09-21 03:25:02 -0800

    complete transaction save method

--- src/repository/jdbc/JdbcTransactionRepository.java
+++ src/repository/jdbc/JdbcTransactionRepository.java
@@ -1,8 +1,10 @@
 package repository.jdbc;
 
+import config.DatabaseConnection;
 import entity.Transaction;
 import repository.TransactionRepository;
 
+import java.sql.*;
 import java.util.List;
 
 public class JdbcTransactionRepository
@@ -10,10 +12,50 @@ public class JdbcTransactionRepository
     @Override
     public void save(Transaction transaction) {
         String sql = """
-        INSERT INTO transactions
-        (user_id, amount, type, description, date)
-        VALUES (?, ?, ?, ?, ?)
-        """;
+                        INSERT INTO transactions
+                (user_id, amount, type, description, date)
+                VALUES (?, ?, ?, ?, ?)
+                """;
+
+
+        try (Connection connection =
+                     DatabaseConnection.getConnection();
+             PreparedStatement statement =
+                     connection.prepareStatement(
+                             sql,
+                             Statement.RETURN_GENERATED_KEYS)) {
+
+            statement.setInt(1,
+                    transaction.getUserId());
+
+            statement.setDouble(2,
+                    transaction.getAmount());
+
+            statement.setString(3,
+                    transaction.getType().name());
+
+            statement.setString(4,
+                    transaction.getDescription());
+
+            statement.setTimestamp(5,
+                    Timestamp.valueOf(
+                            transaction.getDate()
+                    ));
+
+            statement.executeUpdate();
+
+            ResultSet resultSet =
+                    statement.getGeneratedKeys();
+
+            if (resultSet.next()) {
+                transaction.setId(
+                        resultSet.getInt(1)
+                );
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
