6ad88f58e7aac98dec328312987b31e91d54764a
Asma <asmasanjari877@gamil.com>
2026-09-21 01:13:09 -0800

    add getters and setters for transaction fields

--- src/entity/Transaction.java
+++ src/entity/Transaction.java
@@ -25,4 +25,52 @@ public class Transaction {
         this.description = description;
         this.date = LocalDateTime.now();
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
 }
