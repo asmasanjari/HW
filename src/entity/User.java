f209f965f53a7da1199e58100f0907517f236042
Asma <asmasanjari877@gamil.com>
2026-09-21 01:05:30 -0800

    add getters and setters for user fields

--- src/entity/User.java
+++ src/entity/User.java
@@ -27,3 +27,62 @@ public class User {
         this.status = AccountStatus.ACTIVE;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(
+            LocalDateTime registrationDate) {
+
+        this.registrationDate = registrationDate;
+    }
+
+    public AccountStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AccountStatus status) {
+        this.status = status;
+    }
+}
+
